07b348a70475dc7ddca42fd2a21c8a622a627e30
shaon3343 <shaon.ashfak@gmail.com>
2014-12-21 16:20:57 +0600

    commit from Eclipse

--- src/secureData/Abc.java
+++ src/secureData/Abc.java
@@ -2,16 +2,19 @@ package secureData;
 
 public class Abc{
 
-private Abc abc = null;
-private Abc(){}
-
-private static Abc a(){
-if(abc== null){
-abc = new Abc();
-}
-return abc;
-
-}
+	private Abc abc = null;
+	private Abc(){}
+	
+	private static Abc a()
+	{
+		if(abc== null)
+		{
+			abc = new Abc();
+		}
+		
+		return abc;
+	
+	}
 }
 
 
